78ff0cfb26813fd7346082529d3b3415aad62102
Ezequel <ezykel225@gmail.com>
2026-04-19 15:11:47 +0800

    homepage adjustment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import { Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
-import { 
-  FaBullhorn, 
-  FaCalendarAlt, 
+import {
+  FaBullhorn,
+  FaCalendarAlt,
 } from 'react-icons/fa'
 import { MdAnnouncement } from 'react-icons/md'
 import { supabase } from '../supabase/supabaseClient'
